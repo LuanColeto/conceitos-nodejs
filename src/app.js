460c0de17d6064b04ab9731b4384c49724e93fb3
Luan Coleto <luancoleto@outlook.com>
2020-06-17 17:30:35 -0300

    Add ID validation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 // const { uuid } = require("uuidv4");
 
@@ -10,6 +10,18 @@ app.use(express.json());
 app.use(cors());
 
 const repositories = [];
+
+function validateRepositoryId (request, response, next) {
+    const { id } = request.params;
+
+    if (!isUuid(id)) {
+        response.status(400).json({message: "ID is not valid"})
+    }
+
+    return next;
+}
+
+app.use('/repositories/:id', validateRepositoryId);
 
 app.get("/repositories", (request, response) => {
     return response.json(repositories)
@@ -31,9 +43,24 @@ app.post("/repositories", (request, response) => {
     return response.json(repository)
 });
 
-app.put("/repositories/:id", (request, response) => {
-
-});
+// app.put("/repositories/:id", (request, response) => {
+//     const { id } = request.params;
+//
+//     const { url, title, techs } = request.body;
+//
+//     const repositoryIndex = repositories.findIndex(repository => repository.id = id);
+//
+//     const repository = {
+//         title,
+//         url,
+//         techs,
+//         likes: 0
+//     };
+//
+//     repositories[repositoryIndex] = repository;
+//
+//     response.json(repository);
+// });
 
 app.delete("/repositories/:id", (request, response) => {
   // TODO
